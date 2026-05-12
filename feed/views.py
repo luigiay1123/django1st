1f309306265f7fe9be68c97fc79de97b7737fcd2
luigiay1123 <wemixmir801@gmail.com>
2026-05-12 21:06:15 +0800

    5. Displaying dynamic posts

--- feed/views.py
+++ feed/views.py
@@ -1,4 +1,10 @@
 from django.views.generic import TemplateView
+from .models import Post
 
 class HomepageView(TemplateView):
   template_name = "home.html"
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['posts'] = Post.objects.all()
+    return context
